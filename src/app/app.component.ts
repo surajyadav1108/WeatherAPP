3b6a4248003d7f8eaaf3a627347f7f5e33daa080
Suraj yadav <harrypotter1108@gmail.com>
2022-12-18 11:16:52 +0530

    Update app.component.ts
    linked Created weather service for the app into the component typescript file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherData } from './models/weather.model';
+import { WeatherserviceService } from './services/weatherservice.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'WheatherApp';
+export class AppComponent implements OnInit  {
+
+
+  myScriptElement: HTMLScriptElement; // try for weatherjs embed
+
+
+  constructor( private wheatherService: WeatherserviceService) {
+    this.myScriptElement = document.createElement("script"); //try for weatherjs embed
+    this.myScriptElement.src = "src/assets/wheather.js"; //try for weatherjs embed
+    document.body.appendChild(this.myScriptElement); //try for weatherjs embed
+  }
+
+  weatherData?: WeatherData;
+
+  ngOnInit(): void{
+    this.wheatherService.getWheatherData('New Delhi')
+    .subscribe({
+      next: (response) => {
+        this.weatherData = response; 
+
+        console.log(response);
+      }
+    });
+    
+  }
 }
+
+
